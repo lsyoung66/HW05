2dff6d009a51cd276bbd196a55eb0c0f689f35e1
lsyoung66 <84892747+lsyoung66@users.noreply.github.com>
2022-04-14 21:37:48 +0900

    Almost done......

--- word_freq.py
+++ word_freq.py
@@ -2,28 +2,29 @@
 import sys
 import re
 
-if __name__ == '__main__':	
-	f = open("sys.argv[1]", "r")
-	num = int(sys.argv[2])
+if __name__ == '__main__':
+        print(sys.argv)
+        f = open(sys.argv[1], "r")
+        num = int(sys.argv[2])
 
-	line = f.readlines()
-	line = re.sub(r"[^a-zA-Z\s]", "", '{}'.format(line))
+        line = f.readlines()
+        line = re.sub(r"[^a-zA-Z\s]", "", '{}'.format(line))
 
-	f.close()
-	
-	D = {}
-	
-	for i in range(0, len(line)):
-		word.extend(line[i].split())
-		
-	for i in range(0, len(word)):
-		if word[i] in D:
-			D[word[i]] += 1
-		else:
-			D[word[i] = 1
-	
-	D_desc = sorted(D.items(), key=lambda x: x[1], reverse=True)
-			  
-	for i in range(0, num):
-			  print("%-20s@20d" % '{}'.format(D_desc))
-	
+        f.close()
+
+        word = []
+        D = {}
+
+        for i in range(0, len(line)):
+                word.extend(line[i].split())
+
+        for i in range(0, len(word)):
+                if word[i] in D:
+                        D[word[i]] += 1
+                else:
+                        D[word[i]] = 1
+
+        D_desc = sorted(D.items(), key=lambda x: x[1], reverse=True)
+
+        for i in range(0, num):
+                          print("{0:<20}{1:<20}".format(D_desc[i][0], D_desc[i][1]))
