fee6f6f1772e7f6adfd1a97e72271b286047e912
lsyoung66 <lsyoung66@naver.com>
2022-04-14 20:01:44 +0900

    Add open() and readlines()

--- word_freq.py
+++ word_freq.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
+import sys
+import re
 
-print("nothing")
+if __name__ == '__main__':
+	argument = sys.argv
+	#del argument[0]
+	print('>>', format(argument))
+
+f = open("textfile.txt", "r")
+
+text = f.readlines()
+text = re.sub(r"[^a-zA-Z\s]", "", '{}'.format(text))
+
+print(text)
+
+f.close()
